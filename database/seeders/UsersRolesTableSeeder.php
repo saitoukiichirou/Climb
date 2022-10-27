a5ed81fa47f6b4ca387f731a9bacbba4cf527077
saitoukiichirou <85376878+saitoukiichirou@users.noreply.github.com>
2022-10-28 08:13:49 +0900

    管理者用、ゲスト用のロール割り当て

--- database/seeders/UsersRolesTableSeeder.php
+++ database/seeders/UsersRolesTableSeeder.php
@@ -15,9 +15,17 @@ class UsersRolesTableSeeder extends Seeder
      */
     public function run()
     {
+        //admin用にロールを割り当て
         $param = [
           'user_id' => '1',
           'role_id' => '1'
+        ];
+        DB::table('role_user')->insert($param);
+        
+        //ゲスト用にロールを割り当て
+        $param = [
+          'user_id' => '2',
+          'role_id' => '2'
         ];
         DB::table('role_user')->insert($param);
     }
